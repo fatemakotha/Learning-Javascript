c7150b5893dc036ae70ffd270603fd80b83e3b26
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-30 08:38:24 +0000

    Using prompt, variables and alert

---
+++ Variables.js
@@ -0,0 +1,7 @@
+//promt takes an input from the user
+promt("What is your name?");//nothing is saved
+
+var myName = "Angela"; //creating a new variable, you can vary this data
+myName = "Kotha"; //stes variable to "Kotha"
+
+alert(myNAme);//Kotha gets printed
