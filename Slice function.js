33f001f24af8540fedc962292ed93d63195e6f5d
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-30 12:20:37 +0000

    Using slice() function

---
+++ Slice function.js
@@ -0,0 +1,9 @@
+// var a = promt("Type a line");// lets assume 23 characters are typed
+// var aOne = a.length();
+// alert(aOne)//prints 23
+// alert("Remaining characters: " + (140 = aOne))//prints 117
+
+var name = "Kotha"
+name.slice(0,1)//gives first character K as it is at the position 0
+name.slice(4,5)//gives K as it is at the position 4
+name.slice(0,3)//gives Koth as  K=0 o=1 t=2 h=3
