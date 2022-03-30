ad3b353d36a9c8260b5a8d9b8db4031196513a55
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-30 14:13:50 +0000

    Creating and calling functions

---
+++ Functions 2.js
@@ -0,0 +1,31 @@
+//function calls a function, getMilk is the name of the var called
+funtion getMilk() {
+    alert("leaveHouse")
+    alert("moveRight")
+    alert("buyMilk")
+    alert("moveRight")
+    alert("leaveHouse")
+    
+}
+//calling a certain function
+getMilk; //executes  alert("leaveHouse")
+                    // alert("moveRight")
+                    // alert("buyMilk")
+                    // alert("moveRight")
+                    // alert("leaveHouse")
+                    
+// OR 
+funtion getMilk() {
+    console.log("leaveHouse")
+    console.log("moveRight")
+    console.log("buyMilk")
+    console.log("moveRight")
+    console.log("leaveHouse")
+    
+}
+//calling a certain function
+getMilk; //executes  alert("leaveHouse")
+                    // alert("moveRight")
+                    // alert("buyMilk")
+                    // alert("moveRight")
+                    // alert("leaveHoconsole.log
---
+++ Functions.js
@@ -0,0 +1,8 @@
+funtion getMilk() {
+    alert("leaveHouse")
+    alert("moveRight")
+    alert("buyMilk")
+    alert("moveRight")
+    alert("leaveHouse")
+    
+}
