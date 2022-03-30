f4db1d6a53825e336f906339a415782d5f0850ab
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-30 12:59:35 +0000

    Using + - / * and %

---
+++ Arithmetic  operators.js
@@ -0,0 +1,7 @@
+6/4 //gives 1.5
+6%4//gives 2
+//WHY is this important? if % is 0 then even number, if modulo is not 0 then odd number
+
+var cost = 3 + 5 * 2//5*2=10, then 3+10=13 gives 13
+//to avoid problems
+var cost = 3 + (5 * 2)
