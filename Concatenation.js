c41fe8201834f773c9e4e92709912d5e831ff781
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-30 10:46:35 +0000

    Variables concatenation

---
+++ Concatenation.js
@@ -0,0 +1,9 @@
+//Concatenation
+alert("Hello" + "World"); //HelloWorld with no space
+alert("Hello" + " " + "World");//Hello World with  a space
+
+//Contatenation using varibles
+var myName = "Kotha";
+var myDogsName = "Cutu";
+
+alert("My name is" + " " + myName + "." + "My dog's name is" + " " + myDogsName);
