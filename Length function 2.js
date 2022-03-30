13d59d8b68867637c495b697d0a2fa81a1c66eab
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-30 11:47:41 +0000

    Debugged

--- Length function 2.js
+++ Length function 2.js
@@ -1,4 +1,4 @@
 var a = promt("Type a line");// lets assume 23 characters are typed
-var aOne = a.length();
+var aOne = a.length;
 alert(aOne)//prints 23
 alert("Remaining characters: " + (140 - aOne))//prints 117
