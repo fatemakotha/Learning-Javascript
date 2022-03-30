6e45007d3b68227805536681aaafd0d5c2c8dd44
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-30 08:43:30 +0000

    Creating variables and printing a message containing variables

---
+++ Variables 2.js
@@ -0,0 +1,3 @@
+var myName = "Kotha";
+var yourName = prompt("What is your name?"); //saves your name to var yourName, value of this variable is what the user types in
+alert("My name is" + myName+ ", welcome to my course" + yourName + "!");//prints My name is Kotha, welcome to my course Xyz
