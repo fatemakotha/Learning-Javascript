1c8ea63c5fa08589383b76f77c45b1e9db779444
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-30 12:26:59 +0000

    Two ways of using slice function

---
+++ Slice function 2.js
@@ -0,0 +1,14 @@
+// var a = promt("Type a line");// lets assume 23 characters are typed
+// var aOne = a.length();
+// alert(aOne)//prints 23
+// alert("Remaining characters: " + (140 = aOne))//prints 117
+
+var name = prompt("Enter characters here: ");
+var cutOff = name.slice(0,31);
+
+alert(cutoff); //pasting dfdfgdsfjhsdgyjtsdzgfvsdfgvbsdfbsdzhj
+// gives us the first 30 dfdfgdsfjhsdgyjtsdzgfvsdfgvbsd
+
+//OR 
+
+alert(prompt("Enter characters here: ")/slice(0,31));// gives us the first 30 dfdfgdsfjhsdgyjtsdzgfvsdfgvbsd
