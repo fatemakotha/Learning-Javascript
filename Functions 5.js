5cf568c1598b65daf7dcb13ab8cfdd15d49e4454
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-04-04 17:57:35 +0000

    Days left calculator using functions

---
+++ Functions 5.js
@@ -0,0 +1,15 @@
+// Create a function that tells you how many days, weeks and months yiu have left too live if you live 90 years
+// Assume 365days in a year, 52 weeks in a year and 12 months in a year
+function lifeInWeeks(age) {
+    var yearsRemaining = 90 - age;
+    var days = yearsRemaining * 365;
+    var weeks = yearsRemaining * 52;
+    var months = yearsRemaining * 12;
+    
+    console.log("You have " + days + "days," + weeks + "weeks and " + months + "left.");
+    
+    
+    
+}
+// here input is 12
+lifeInWeeks(12);
