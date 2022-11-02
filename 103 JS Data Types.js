d5584fa19b3ec7b1f1409301e7e4266afe61924e
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 05:21:01 +0000

    103 JS Data Types.js

---
+++ 103 JS Data Types.js
@@ -0,0 +1,11 @@
+// 1-Data types:
+// strings: "rfsdf", numbers: 1 2 , booleans: true/false
+alert(2+3); //pops up 5 in alert box
+alert(2*3); // 6
+alert(2**3); //Exponentiation 2^3 which is 8, pops 8
+alert(13%3); //Modulus(remainder) 3*4 = 12, 13-12 = 1 pops 1
+
+// 2-The typeof() command:
+typeof("hi"); // prints: 'string'
+typeof(1); // prints: 'number'
+typeof(true); // prints: 'boolean'
