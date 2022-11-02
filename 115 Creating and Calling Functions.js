eb38b956a15b28d406ae666bdb2149b384152010
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 05:32:49 +0000

    Functions

---
+++ 115 Creating and Calling Functions.js
@@ -0,0 +1,13 @@
+// 1-Can not start the name of function with a number like 1getmilk()
+function getMilk(){
+    alert("leave house")
+    alert("go out")
+    alert("give money")
+    alert("bring milk")   
+} //no ; needed here at the end of function
+
+getMilk(); //we do need ; while calling the function though
+
+
+// 2-Different between console.log() and alert() is that the console can only be seen by the developer but the alert
+//can be seen by the user as well as the developer
