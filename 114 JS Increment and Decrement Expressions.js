fc3952887163892c2e22c821973f64e709f82f41
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 05:32:06 +0000

    Increment and decrement

---
+++ 114 JS Increment and Decrement Expressions.js
@@ -0,0 +1,20 @@
+// 1-Increment
+var x = 5;
+x = x + 1; //prints 6
+x++; //prints 6
+
+// 2-Decrement
+x = x - 1; //prints 4
+x--;
+
+
+// 3- Inc and dec:
+x += y //x is incremented by y
+x -+ y //x is decremented by y
+
+//Quiz: ****
+//What does y  equal?
+var x = 3;
+var y = x++;
+y += 1; // prints 4 
+//In this line: var y = x++ the value of x is assigned to y before x is //incremented, so y equals 3 on line 2, while x equals 4. There fore on line 3, y //now equals 4 instead of 5.
