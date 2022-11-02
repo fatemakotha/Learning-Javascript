43988162183e874103328706c071ec3247bb6893
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 05:23:32 +0000

    Naming

---
+++ 107 JS Naming Conventions.js
@@ -0,0 +1,16 @@
+// 1-Press ctrl+l in windows and command+k in mac to clear the console.
+
+// 2-Press on refresh of browser and hold then choose Empty Cache and Hard Reload to clear calls
+
+// 3-Naming conventions:
+var myName = "Kotha";
+
+// 4-Cannot name a variable with the same name as a built in function
+// var var = "kOTHA" will give ERROR
+
+// 5-These names are fine:
+//var my134 = 134
+
+// 6-Names can only have letters, numbers, _ and $
+
+//7-It is nice to use camelCasing: myName or myProgramIsCool
