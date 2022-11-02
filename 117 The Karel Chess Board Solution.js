ce8d990b187c2e5d67a5af1c21f29b1099740526
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 05:34:39 +0000

    Karel

---
+++ 117 The Karel Chess Board Solution.js
@@ -0,0 +1,43 @@
+// 1-Karel chessboard solution:
+function main(){
+   beepersRight();
+   goUpTurnLeft();
+   beepersLeft();
+   goUpTurnRight();
+      beepersRight();
+   goUpTurnLeft();
+   beepersLeft();
+   goUpTurnRight();
+      beepersRight();
+}
+ 
+function goUpTurnRight() {
+   turnRight();
+   move();
+   turnRight();
+}
+ 
+function goUpTurnLeft() {
+   turnLeft();
+   move();
+   turnLeft();
+}
+ 
+function beepersRight() {
+   putBeeper();
+   move();
+   move();
+   putBeeper();
+   move();
+   move();
+   putBeeper();  
+}
+ 
+function beepersLeft() {
+   move();
+   putBeeper();
+   move();
+   move();
+   putBeeper();
+   move();
+}
