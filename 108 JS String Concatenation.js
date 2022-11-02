5602dd167813c013575234dd7868f6c3dbf5b361
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 05:24:17 +0000

    String concatenation

---
+++ 108 JS String Concatenation.js
@@ -0,0 +1,6 @@
+// 1-String concatenation:
+var message = "Hello";
+var name = "Angela";
+
+alert("Here is " + message + " " + name) //pops Here is Hello Angela
+console.log("Here is " + message + " " + name) //Here is prints Hello Angela
