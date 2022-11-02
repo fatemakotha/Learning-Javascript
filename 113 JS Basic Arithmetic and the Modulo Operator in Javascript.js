07cf48ab38e5f8932fd20ebfbbc18966a3322ffd
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 05:31:13 +0000

    Basic Arithmetic and the Modulo Operator in Javascript

---
+++ 113 JS Basic Arithmetic and the Modulo Operator in Javascript.js
@@ -0,0 +1,14 @@
+var cost = 3 + (5 * 2)
+
+alert(cost) //alerts 13
+
+
+
+//Convert dog age to human age:
+dogAge = prompt("Enter dog's age: ")
+
+alert(typeof(dogAge)) // is a string but in the next line of code we add an int to it ***
+
+humanAge = ((dogAge - 2) * 4) + 21;
+
+alert(humanAge)
