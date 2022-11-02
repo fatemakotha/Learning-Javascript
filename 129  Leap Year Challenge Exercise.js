a690b918aa68f88a1ca0e4adc160fe9f953fb2bf
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 07:53:50 +0000

    Program to check leap year

--- 129  Leap Year Challenge Exercise.js
+++ 129  Leap Year Challenge Exercise.js
@@ -2,7 +2,7 @@
 function checkLeapYear(year) {
 
     //three conditions to find out the leap year
-    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+    if ((0 === year % 4) && (0 != year % 100) || (0 === year % 400)) {
         console.log(year + ' is a leap year');
     } else {
         console.log(year + ' is not a leap year');
@@ -13,3 +13,25 @@ function checkLeapYear(year) {
 const year = prompt('Enter a year:');
 
 checkLeapYear(year);
+
+
+//------------OR----------------------------------
+
+// program to check leap year
+function isLeap(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return "Leap year";
+            } else {
+                return "Not leap year";
+            }
+        } else {
+            return "Leap year";
+        }
+    } else {
+        return "Not leap year";
+    }
+}
+
+isLeap(2016);
