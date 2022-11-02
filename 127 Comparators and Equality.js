08dff221a5e9359a56112f41035739a29ff2b561
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-11-02 06:39:12 +0000

    Comparators and equality

---
+++ 127 Comparators and Equality.js
@@ -0,0 +1,25 @@
+// === IS EQUAL TO
+// !== NOT EQUAL 2
+// > GREATER THAN
+// < SMALLER THAN
+// <= SMALLER THAN OR EQUAL TO
+// >= SMALLER THAN OR EQUAL TO
+
+var a = 1 //number
+var b = "1" //string
+
+
+// 1-Checks if the data types are matching: **
+if (a === b) {
+    console.log("yes")
+} else {
+    console.log("NOT")
+} //prints NOT
+
+
+// 2-Checks only if the data values are matching: **
+if (a == b) {
+    console.log("yes")
+} else {
+    console.log("no")
+} //prints yes
